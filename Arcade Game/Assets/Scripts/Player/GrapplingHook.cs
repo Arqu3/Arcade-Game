fbd0c9f9bb8d1170b833639601f3f5bd3cfecce3
Arqu3 <thesparvgames@gmail.com>
2016-04-02 19:27:27 +0200

    Added basic grappling hook and updated jumping mechanics

---
+++ Arcade Game/Assets/Scripts/Player/GrapplingHook.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrapplingHook : MonoBehaviour {
+
+    public GameObject hookPrefab;
+    public float speed = 20.0f;
+    public float totalVelocity;
+
+    Vector3 relativePlayerPos;
+    public Vector3 direction;
+    public bool hasShot = false;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        relativePlayerPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        direction = Input.mousePosition - relativePlayerPos;
+
+        if (Input.GetMouseButtonDown(0) && !hasShot)
+        {
+            Shoot();
+        }
+
+        totalVelocity = hookPrefab.GetComponent<Rigidbody2D>().velocity.magnitude;
+
+    }
+
+    void Shoot()
+    {
+        hasShot = true;
+        GameObject clone;
+        clone = (GameObject)Instantiate(hookPrefab, transform.position, Quaternion.identity);
+
+        float x = direction.x;
+        float y = direction.y;
+
+        float squareX = x * x;
+        float squareY = y * y;
+
+        float added = squareX + squareY;
+
+        Mathf.Sqrt(added);
+
+        Vector3 unit = new Vector3((x / added), (y / added), 0);
+
+        clone.GetComponent<Rigidbody2D>().AddForce(unit * speed);
+    }
+}
--- Arcade Game/Assets/Scripts/Player/PlayerMovement.cs
+++ Arcade Game/Assets/Scripts/Player/PlayerMovement.cs
@@ -14,21 +14,17 @@ public class PlayerMovement : MonoBehaviour {
 	// Use this for initialization
 	void Start()
 	{
-	
 	}
 	
-	// Update is called once per frame
+	//Fixed update
 	void FixedUpdate()
 	{
         float hSpeed = Input.GetAxis("Horizontal");
 
         isOnGround = Physics2D.OverlapCircle(groundCheck.position, 0.2f, whatIsGround);
 
-        //Physics-based movement
-        //GetComponent<Rigidbody2D>().velocity = new Vector2(hSpeed * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
-
         //Non-physics-based movement
-        float x = hSpeed * maxSpeed;
+        float x = hSpeed * maxSpeed * Time.deltaTime;
         transform.position = new Vector2(transform.position.x + x, transform.position.y);
 
         //Reload level if outside screen
@@ -38,11 +34,22 @@ public class PlayerMovement : MonoBehaviour {
         }
 	}
 
+    //Regular update
     void Update()
     {
         if (isOnGround && Input.GetKeyDown(KeyCode.Space))
         {
             GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, jumpVelocity));
+        }
+
+        Debug.Log(GetComponent<Rigidbody2D>().velocity.y);
+        if (GetComponent<Rigidbody2D>().velocity.y > 0)
+        {
+            Physics2D.IgnoreLayerCollision(8, 10, true);
+        }
+        else
+        {
+            Physics2D.IgnoreLayerCollision(8, 10, false);
         }
     }
 }
